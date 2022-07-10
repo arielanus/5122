ca00a4c8ee2803e0b6e502bd85a5f9775f5777fc
Ariel <arielanus1980@yahoo.com.ar>
2022-07-10 12:39:15 -0300

    segundo ejercicio

---
+++ script.js
@@ -0,0 +1,4 @@
+var edad = Number(prompt("Ingrese su edad"))
+var colorRemera = prompt("Ingrese color")
+const color=("rojo")
+if (edad > 18 && colorRemera==color) {alert("Puede ingresar al bar")}else{("Acceso denegado")}
